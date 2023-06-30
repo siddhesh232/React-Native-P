12afaf381428a0fba98a2df50851ddeac8cf20a6
appxbuild <dandekarsiddhesh.02@gmail.com>
2023-06-30 18:54:34 +0530

    Started Section 10: Sending HTTP Request - Sending POST request and fetching backend data (GET Request)

--- Section9/screens/ManageExpense.js
+++ Section9/screens/ManageExpense.js
@@ -1,11 +1,11 @@
 import { useContext, useLayoutEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import { ExpensesContext } from '../store/expenses-context';
 import ExpenseForms from "../components/ManageExpense/ExpenseForms";
+import {storeExpense} from "../util/http";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -35,6 +35,7 @@ function ManageExpense({ route, navigation }) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
+      storeExpense(expenseData);
       expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
