f21a1da3bcbf407cda90834e6973a7ab87fb8954
appxbuild <dandekarsiddhesh.02@gmail.com>
2023-07-06 19:24:49 +0530

    ..

--- UserAuthentication/screens/WelcomeScreen.js
+++ UserAuthentication/screens/WelcomeScreen.js
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState(" ");
+
+  useEffect(() => {
+    axios.get(
+        'https://rncourse-db-default-rtdb.firebaseio.com/message.json');
+    },[]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text></Text>
     </View>
   );
 }
